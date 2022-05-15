bd6a3a9ca9ae7a585f2aede0ce1b5affa8ab1a74
prot1vogas <lakeev.s.u@gmail.com>
2022-05-15 20:19:50 +0300

    swagger, user register route config

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,9 +6,13 @@ use App\Entity\Dto\RegisterDto;
 use App\Service\RegisterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
 
+#[Route('/user')]
 class UserController extends AbstractController
 {
     private SerializerInterface $serializer;
@@ -20,7 +24,8 @@ class UserController extends AbstractController
         $this->registerService = $registerService;
     }
 
-    public function register($request)
+    #[Route('/register/', name: 'user_register', methods: 'POST')]
+    public function register(Request $request): Response
     {
         $registerDto = $this->serializer->deserialize($request->getContent(), RegisterDto::class, JsonEncoder::FORMAT);
         $user = $this->registerService->register($registerDto);
